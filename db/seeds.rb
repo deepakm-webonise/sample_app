758dc5f0373e0db6bf83da41da02238c086b4ab9
Deepak Mahakale <deepakmahakale@gmail.com>
2015-01-21 21:46:19 +0530

    Finish user edit delete update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the
+# db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  'Deepak Mahakale',
+             email: 'deepak.m@weboniselab.com',
+             password:              'foobar',
+             password_confirmation: 'foobar',
+             admin: true)
+
+50.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n + 1}@weboniselab.com"
+  password = 'password'
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
